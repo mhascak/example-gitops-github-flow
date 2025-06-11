50e00ea26887076218b8bdde38b8175a250c559b
Martin Haščák <martinhascak@gmail.com>
2025-06-11 11:57:24 +0200

    Create 202506111157_create_table_test_ddl.sql

---
+++ migrations-semver/202506111157_create_table_test_ddl.sql
@@ -0,0 +1,13 @@
+IF OBJECT_ID('dbo.test', 'U') IS NULL
+BEGIN
+    CREATE TABLE [dbo].[test] (
+        [ProjectId] INT IDENTITY(1,1) PRIMARY KEY,
+        [Name] NVARCHAR(100) NOT NULL,
+        [Description] NVARCHAR(500) NULL,
+        [StartDate] DATE NOT NULL,
+        [EndDate] DATE NULL,
+        [Status] NVARCHAR(50) NOT NULL DEFAULT 'New',
+        [CreatedAt] DATETIME2 NOT NULL DEFAULT GETDATE(),
+        [UpdatedAt] DATETIME2 NULL
+    );
+END
