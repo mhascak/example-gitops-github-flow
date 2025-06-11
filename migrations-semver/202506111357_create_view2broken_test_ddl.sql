d8301f81d3d91e6737f23e34b959738376926b29
Martin Haščák <martinhascak@gmail.com>
2025-06-11 13:58:06 +0200

    Create 202506111357_create_view2broken_test_ddl.sql

---
+++ migrations-semver/202506111357_create_view2broken_test_ddl.sql
@@ -0,0 +1,17 @@
+IF OBJECT_ID('dbo.vw_test2', 'V') IS NOT NULL
+    DROP VIEW dbo.vw_test2;
+GO
+
+ VIEW dbo.vw_test2 AS
+SELECT
+    ProjectId,
+    Name,
+    Description,
+    StartDate,
+    EndDate,
+    Status,
+    CreatedAt,
+    UpdatedAt
+FROM
+    dbo.test;
+GO
